1487a8f94b8f83e52f1e1eabfcba0d67a23dccbb
theletterrsquared@gmail.com <theletterrsquared@gmail.com>
2016-01-05 18:46:26 -0800

    was having trouble figuring out what you wanted from the other tests

--- username.rb
+++ username.rb
@@ -2,13 +2,24 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+	if(first.empty? || last.empty?)
+		return nil
+	end
+	first.gsub!(/\s|\W|\d/, "")
+	last.gsub!(/\s|\W|\d/, "")
+	return (first[0]<<last).downcase
 end
 
-def format_year
-  nil
+def format_year(year)
+	if(year.to_s.size != 4)
+		return nil
+	end
+  	y = year.to_s 
+  	return y[2]<<y[3]
 end
 
-def build_username
-  nil
+def build_username(first, last, year)
+  year.to_s!
+  p year
+  return (first[0]<<last).downcase<<year[2]<<year[3]
 end
--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,22 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase<<"!"
 end
 
-def first_char
-  "z"
+def first_char(word)
+  return word[0].downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna(word)
+  # "crackercrackercracker"
+  return word * 3
 end
+
+def after_you
+	return 'no, after you'
+end	
